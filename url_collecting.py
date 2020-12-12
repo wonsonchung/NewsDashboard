a730f68b2687f6df9553eda1ab81e88738fbb565
sieun.lee <sieun.lee@voithru.com>
2020-12-12 14:33:36 +0900

    feat: get datetime and category as argument in main_crawler

--- url_collecting.py
+++ url_collecting.py
@@ -1,9 +1,28 @@
 from NewsCrawler import url_crawler
+from NewsCrawler.category import categories, reversed_categories
+import argparse
 
-categories = {'정치': 100, '경제': 101, '사회': 102, '생활문화': 103, '세계': 104, 'IT과학': 105, '오피니언': 110}
-keys = list(categories.keys())
+parser = argparse.ArgumentParser(description='Date Configuration')
+parser.add_argument('--start', required=True, help='start date of crawling ex.0901')
+parser.add_argument('--end', required=True, help='end date of crawling ex.0910')
+parser.add_argument('--category', required=False, help='')
+
+args = parser.parse_args()
+
+start_month, start_day = int(args.start[:2]), int(args.start[2:])
+end_month, end_day = int(args.end[:2]), int(args.end[2:])
+
+category = args.category.strip().split(',')
+
+for i, cate in enumerate(category):
+    if cate in categories:
+        continue
+    elif int(cate) in reversed_categories:
+        category[i] = reversed_categories[int(cate)]
+    else:
+        raise KeyError
+
 Crawler = url_crawler.UrlCrawler()
-
-Crawler.set_category('정치', '경제','사회', '생활문화', '세계', 'IT과학', '오피니언')
-Crawler.set_date_range(9, 1, 9, 30)
+Crawler.set_category(category)
+Crawler.set_date_range(start_month, start_day, end_month, end_day)
 Crawler.start()
